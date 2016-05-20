d229e1dbba64bcb61fc92a4c40adafa00fca67c3
Jacopo Di Stanislao <jacopo.distanislao@yoox.com>
2016-05-20 12:49:28 +0200

    Add fold function

--- hhfuns.erl
+++ hhfuns.erl
@@ -1,5 +1,5 @@
 -module(hhfuns).
--export([map/2, incr/1, decr/1]).
+-export([map/2, incr/1, decr/1, fold/3]).
 
 map(_,[]) -> [];
 map(F, [H|T]) -> [F(H)|map(F,T)].
@@ -10,3 +10,10 @@ decr(X) -> X - 1.
 %% L = [1,2,3,4].
 %% hhfuns:map(fun hhfuns:incr/1, L).
 %% hhfuns:map(fun(X) -> X * 2 end, L).
+
+fold(_, Start, []) -> Start;
+fold(F, Start, [H|T]) -> fold(F, F(H, Start), T).
+
+%% [H|T] = [1,2,3,4].
+%% hhfuns:fold(fun(A,B) -> A + B end, 0, T).
+%% hhfuns:fold(fun(A,B) -> A + B end, H, T).
