a187c16e4c8aeb2efd93487b35977411b0769361
Jacopo Di Stanislao <jacopo.distanislao@yoox.com>
2016-06-15 18:22:35 +0200

    Add my_time_func(F) to bifs.erl

--- ProgrammingErlang/Chapter_4/bifs.erl
+++ ProgrammingErlang/Chapter_4/bifs.erl
@@ -1,8 +1,19 @@
 -module(bifs).
 
 %% API
--export([my_tuple_to_list/1]).
+-export([my_tuple_to_list/1, my_time_func/1, my_date_string/0]).
 
 
 my_tuple_to_list(T) when is_tuple(T) ->
   [element(N, T) || N <- lists:seq(1, tuple_size(T))].
+
+my_time_func(F) ->
+  Start = erlang:system_time(milli_seconds),
+  F(),
+  erlang:system_time(milli_seconds) - Start.
+
+my_date_string() ->
+  {Hour, Minute, Second} = time(),
+  {Year, Month, Day} = date(),
+  io:fwrite(standard_io, "Year: ~w, Month: ~w, Day: ~w at ~w:~w:~w",[Year, Month, Day, Hour, Minute, Second]).
+
